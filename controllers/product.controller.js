a9b04b0745d7736413fb388be64fd1628c07260c
ibrahim_3attef <ibrahimhelaly386@gmail.com>
2026-09-18 19:52:17 +0300

    refactor: improve product controller structure

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,22 +1,30 @@
 const Product = require('../models/product.model')
+const appError = require('../utils/appError');
+const asyncWrapper = require('../middlewares/asyncWrapper');
+const jSend = require("../utils/Jsendvar")
 
-const getProducts = async (req , res)=>{
+const getProducts = asyncWrapper (
+    async (req , res)=>{
     const data = await Product.find();
     res.status(200).json({status:"success" , data: {data}})
-}
+})
 
-const getProduct = async (req , res)=>{
+const getProduct = asyncWrapper (async (req , res)=>{
     const productId = req.params.id;
     const product = await Product.findById(productId);
+    if(!product){
+        const error = appError.create("Product Not Found" , 400 , jSend.ERROR);
+        return next(error)
+    }
     res.status(200).json({status:"success" , data: {product}})
-}
+})
 
-const postProduct = async (req , res)=>{
-    console.log(req.body)
+const postProduct = asyncWrapper (
+    async (req , res)=>{
     const createProduct = new Product(req.body);
     await createProduct.save()
     res.status(201).json({status: "success" , data: {msg : "User Created"}})
-}
+})
 
 module.exports = {
     getProducts,
