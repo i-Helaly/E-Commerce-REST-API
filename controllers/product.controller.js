e02921985d145673f18c33ca7cc0f01c5d750541
ibrahim_3attef <ibrahimhelaly386@gmail.com>
2026-09-18 20:13:16 +0300

    feat: add product update and delete

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -9,9 +9,20 @@ const getProducts = asyncWrapper (
     res.status(200).json({status:"success" , data: {data}})
 })
 
-const getProduct = asyncWrapper (async (req , res)=>{
+const getProduct = asyncWrapper (async (req , res , next)=>{
     const productId = req.params.id;
     const product = await Product.findById(productId);
+    if(!product){
+        const error = appError.create("Product Not Found" , 400 , jSend.ERROR);
+        return next(error)
+    }
+    res.status(200).json({status:"success" , data: {product}})
+})
+
+const updateProduct = asyncWrapper (
+    async (req , res , next)=>{
+    const productId = req.params.id;
+    const product = await Product.findByIdAndUpdate(productId , req.body , {new : true});
     if(!product){
         const error = appError.create("Product Not Found" , 400 , jSend.ERROR);
         return next(error)
@@ -26,8 +37,24 @@ const postProduct = asyncWrapper (
     res.status(201).json({status: "success" , data: {msg : "User Created"}})
 })
 
+const deleteProduct = asyncWrapper (
+    async (req , res , next)=>{
+    const productId = req.params.id;
+    const product = await Product.findByIdAndDelete(productId);
+    if(!product){
+        const error = appError.create("Product Not Found" , 400 , jSend.ERROR);
+        return next(error)
+    }
+    res.status(200).json({status:"success" , data: null})
+})
+
+
+
+
 module.exports = {
     getProducts,
     getProduct,
     postProduct,
+    updateProduct,
+    deleteProduct
 }
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -7,5 +7,7 @@ const router = express.Router();
 router.get('/api/product'  ,productsController. getProducts)
 router.get('/api/product/:id' , productsController.getProduct)
 router.post('/api/product' ,bodyOfValidation,resOfValidation, productsController.postProduct)
+router.patch('/api/product/:id' ,productsController.updateProduct)
+router.delete('/api/product/:id' ,productsController.deleteProduct)
 
 module.exports = router;
